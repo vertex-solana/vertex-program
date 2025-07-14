6764228d369d0f6f18f2c7a0f9ea9882b7108e25
minhbear <minhbeardev@gmail.com>
2025-07-15 03:42:13 +0700

    feat: init user vault ix

--- programs/vertex-program/src/states/user_vault.rs
+++ programs/vertex-program/src/states/user_vault.rs
@@ -18,6 +18,7 @@ pub struct UserVault {
   pub storage_last_billed_slot: u64,
   pub read_debts: [ReadDebt; 5],
   pub billing_status: Option<BillingStatus>,
+  pub rent_lamports: u64,
 }
 
 #[derive(Debug, InitSpace, Clone, Copy, AnchorDeserialize, AnchorSerialize, Default)]
@@ -28,12 +29,13 @@ pub struct ReadDebt {
 }
 
 impl UserVault {
-  pub fn init(&mut self, owner: Pubkey, bump: u8) {
+  pub fn init(&mut self, owner: Pubkey, bump: u8, rent_lamports: u64) {
     self.owner = owner;
     self.bump = bump;
     self.storage_bytes = 0;
     self.storage_last_billed_slot = u64::try_from(Clock::get().unwrap().slot).unwrap();
     self.read_debts = [ReadDebt::default(); MAX_READ_DEBT];
     self.billing_status = None;
+    self.rent_lamports = rent_lamports;
   }
 }
