8a52fe1aa92d9564deb25ea212ac3cf755f25c0a
minhbear <minhbeardev@gmail.com>
2025-07-24 01:55:33 +0700

    chore: update keypair contract and deployer key for devnet

--- programs/vertex-program/src/program_id.rs
+++ programs/vertex-program/src/program_id.rs
@@ -4,8 +4,8 @@ cfg_if::cfg_if! {
   if #[cfg(feature = "staging")] {
     pub const PROGRAM_ID: Pubkey = pubkey!("ENQJbjpnrTJus45f5kUg5M2LN9Sozf7JfZ9nAV3GVNZD");
   } else if #[cfg(feature = "dev")] {
-    pub const PROGRAM_ID: Pubkey = pubkey!("ENQJbjpnrTJus45f5kUg5M2LN9Sozf7JfZ9nAV3GVNZD");
+    pub const PROGRAM_ID: Pubkey = pubkey!("AtanB6GFaMXuM8mBUgSJUMwtKEB7ii35LAUwAQwdEsFf");
   } else {
-    pub const PROGRAM_ID: Pubkey = pubkey!("ENQJbjpnrTJus45f5kUg5M2LN9Sozf7JfZ9nAV3GVNZD");
+    pub const PROGRAM_ID: Pubkey = pubkey!("AtanB6GFaMXuM8mBUgSJUMwtKEB7ii35LAUwAQwdEsFf");
   }
 }
